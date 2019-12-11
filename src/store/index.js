01a6ef5b56245ed83a812603b50c5d6abc6110dc
Faye <lis@rose-hulman.edu>
2019-12-11 16:30:15 +0800

    mock api response

--- src/store/index.js
+++ src/store/index.js
@@ -6,8 +6,8 @@ Vue.use(Vuex)
 
 export const actions = {
   async getData(store) {
-    const response = await axios.get('https://jsonplaceholder.typicode.com/posts/1')
-    store.commit('SET_DATA', { id: response.data.id })
+    const response = await axios.get('https://mymockendpoint.com/projects/1')
+    store.commit('SET_DATA', { projectId: response.data.projectId })
   }
 }
 
--- tests/unit/actions.spec.js
+++ tests/unit/actions.spec.js
@@ -1,9 +1,15 @@
 import { actions } from '../../src/store/index.js'
 
+jest.mock('axios', () => {
+    return {
+        get: () => ({ data: { projectId: '8182fad6'}})
+    }
+})
+
 describe('getData', () => {
     it('makes an API and commits the response', async () => {
         const store = { commit: jest.fn() }
         await actions.getData(store)
-        expect(store.commit).toHaveBeenCalledWith('SET_DATA', { id: 1 })
+        expect(store.commit).toHaveBeenCalledWith('SET_DATA', { projectId: '8182fad6'})
     })
 })
