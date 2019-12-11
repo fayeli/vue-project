418655a7a34f8857985b10735389868947a30540
Faye <lis@rose-hulman.edu>
2019-12-11 21:12:49 +0800

    unread notifications ui

---
+++ src/components/Notifications.vue
@@ -0,0 +1,43 @@
+<template>
+    <v-container fluid class="text-center">
+      <v-row
+        justify="space-around"
+      >
+        <div>
+          <v-btn
+            class="mx-1"
+            color="primary"
+            @click="unread++"
+          >
+            New Notification
+          </v-btn>
+          <v-btn
+            class="mx-1"
+            color="error"
+            @click="unread = 0"
+          >
+            Read All
+          </v-btn>
+        </div>
+        <v-badge
+          color="green"
+          overlap
+        >
+          <template v-slot:badge>
+            <span v-if="unread > 0">{{ unread }}</span>
+          </template>
+          <v-icon large>mdi-bell</v-icon>
+        </v-badge>
+      </v-row>
+    </v-container>
+</template>
+
+<script>
+export default {
+    name: 'Notifications',
+
+    data: () => ({
+        unread: 2
+    }),
+};
+</script>
