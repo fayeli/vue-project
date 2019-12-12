bebba1c5859dd9d1f40ab61c91657535c957ee39
Faye <lis@rose-hulman.edu>
2019-12-12 10:21:09 +0800

    display new results in table after refreshing data

--- src/App.vue
+++ src/App.vue
@@ -3,6 +3,15 @@
     <v-content>
       <NotificationSystem/>
       <ProjectsTable/>
+      <v-container fluid>
+        <v-btn
+          class="mx-1"
+          color="primary"
+          @click="refreshData()"
+        >
+          Refresh Data
+        </v-btn>
+      </v-container>
     </v-content>
   </v-app>
 </template>
@@ -26,6 +35,12 @@ export default {
 
   created() {
     this.$store.dispatch('getData');
+  },
+
+  methods: {
+    refreshData: function() {
+      this.$store.dispatch('getData');
+    }
   }
 };
 </script>
--- tests/e2e/specs/test.js
+++ tests/e2e/specs/test.js
@@ -1,38 +1,58 @@
 // https://docs.cypress.io/api/introduction/api.html
+mockData = [
+  {
+      projectName: 'Test title 1',
+      projectDescription: 'Test description',
+      featured: 1,
+      categoryName: 'Test Category',
+      fundingGoal: '100000',
+      percentageComplete: '50'
+  },
+  {
+      projectName: 'Test title 2',
+      projectDescription: 'Test description',
+      featured: 0,
+      categoryName: 'Test Category',
+      fundingGoal: '100000',
+      percentageComplete: '90'
+  },
+  {
+      projectName: 'Test title 3',
+      projectDescription: 'Test description',
+      featured: 0,
+      categoryName: 'Test Category',
+      fundingGoal: '100000',
+      percentageComplete: '20'
+  }
+];
 
 describe('My First Test', () => {
+
   it('Should consume an API and display the results', () => {
     cy.server()
-    
-    cy.route('https://jsonplaceholder.typicode.com/posts', [
-        {
-            projectName: 'Test title 1',
-            projectDescription: 'Test description',
-            featured: 1,
-            categoryName: 'Test Category',
-            fundingGoal: '100000',
-            percentageComplete: '50'
-        },
-        {
-            projectName: 'Test title 2',
-            projectDescription: 'Test description',
-            featured: 0,
-            categoryName: 'Test Category',
-            fundingGoal: '100000',
-            percentageComplete: '90'
-        },
-        {
-            projectName: 'Test title 3',
-            projectDescription: 'Test description',
-            featured: 0,
-            categoryName: 'Test Category',
-            fundingGoal: '100000',
-            percentageComplete: '20'
-        }
-      ]
-    )
+    cy.route('https://jsonplaceholder.typicode.com/posts', mockData)
     cy.visit('/')
-    cy.contains('div', 'Projects')
+    cy.contains('td', 'Test Title 1')
+    cy.contains('td', 'Test Title 2')
+    cy.contains('td', 'Test Title 3')
+  })
+
+  it('Should display new results after repolling API', () => {
+    cy.server()
+    mockData.push({
+        projectName: 'Test title 4',
+        projectDescription: 'Test description',
+        featured: 0,
+        categoryName: 'Test Category',
+        fundingGoal: '100000',
+        percentageComplete: '20'
+    })
+    cy.route('https://jsonplaceholder.typicode.com/posts', mockData)
+    cy.contains('Refresh Data').click()
+    cy.contains('td', 'Test Title 1')
+    cy.contains('td', 'Test Title 2')
+    cy.contains('td', 'Test Title 3')
+    cy.contains('td', 'Test Title 4')
   })
   
 })
